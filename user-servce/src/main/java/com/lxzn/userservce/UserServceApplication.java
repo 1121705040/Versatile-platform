e5d938e00af4cf5bd68db7ca747400d59d8b8b9e
lixin <1121705040@qq.com>
2024-04-29 20:38:01 +0800

    项目构建

---
+++ user-servce/src/main/java/com/lxzn/userservce/UserServceApplication.java
@@ -0,0 +1,13 @@
+package com.lxzn.userservce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServceApplication.class, args);
+    }
+
+}
